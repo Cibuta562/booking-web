947afd8b894265c5f44e0cca245dad85721a82c3
Boboc Robert Andrei <andreiboboccibo@gmail.com>
2022-09-12 12:43:22 +0300

    MEGA IMAGE

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import AllBookings from "./pages/AllBookings";
+import NewBooking from "./pages/NewBooking";
+import Favorites from "./pages/Favorites";
+import MainNav from "./components/layout/MainNav";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<MainNav />} />
+            <Route path="/all-bookings" element={<AllBookings />} />
+            <Route path="/new-booking" element={<NewBooking />} />
+            <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ src/pages/Favorites.js
@@ -0,0 +1,12 @@
+import MainNav from "../components/layout/MainNav";
+
+function Favorites () {
+    return(
+        <div>
+            <MainNav/>
+            Favorites
+    </div>
+    );
+}
+
+export default Favorites;
