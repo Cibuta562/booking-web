6a44ab91a43bed41dc02bbc2d29b0190dd9e492f
Boboc Robert Andrei <andreiboboccibo@gmail.com>
2022-09-13 13:26:00 +0300

    HAI MEGA IMAGE

--- src/index.js
+++ src/index.js
@@ -2,22 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import MainNav from "./components/layout/MainNav";
 import AllBookings from "./pages/AllBookings";
 import NewBooking from "./pages/NewBooking";
 import Favorites from "./pages/Favorites";
 
 
-ReactDOM.render(
-    <BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+    <HashRouter>
         <Routes>
             <Route path="/" element={<MainNav />} />
             <Route path="/all-bookings" element={<AllBookings />} />
             <Route path="/new-booking" element={<NewBooking />} />
             <Route path="/favorites" element={<Favorites />} />
         </Routes>
-    </BrowserRouter>, document.getElementById("root"));
+    </HashRouter>
+    </React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
